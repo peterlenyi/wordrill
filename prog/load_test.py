bc84b1e4efd41308ea829fbeb06c03f7fada6485
Peter Lényi <mgr.peter.lenyi@mailbox.org>
2025-07-27 18:57:32 +0200

    fix: not parsing comments in dictionaries

--- prog/load_test.py
+++ prog/load_test.py
@@ -7,15 +7,13 @@ from prog.load import parse_information_dictionary, parse_translation_dictionary
 def test_parse_information_dictionary():
     sample = "\n"
     sample += "[animals]\n"
-    sample += "cat = pl. cats\n"
+    sample += "# cat = pl. cats\n"
     sample += "dog = pl. dogs\n"
-    sample += "fish = pl. fish\n"
+    sample += "; fish = pl. fish\n"
     sample += "mouse = pl. mice\n"
 
     wanted = {
-        'cat': 'pl. cats',
         'dog': 'pl. dogs',
-        'fish': 'pl. fish',
         'mouse': 'pl. mice'
     }
 
@@ -27,15 +25,13 @@ def test_parse_information_dictionary():
 def test_parse_translation_dictionary():
     sample = "\n"
     sample += "[animals]\n"
-    sample += "cat = Katze, Kater\n"
+    sample += "# cat = Katze, Kater\n"
     sample += "dog = Hund, Hündin\n"
-    sample += "fish = Fisch\n"
+    sample += "; fish = Fisch\n"
     sample += "mouse = Maus\n"
 
     wanted = {
-        'cat': ['Katze', 'Kater'],
         'dog': ['Hund', 'Hündin'],
-        'fish': ['Fisch'],
         'mouse': ['Maus']
     }
 
